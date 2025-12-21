282fa51ccf9860434ca9438b8e14a8406b789750
emilmiku <emilija.mikuliene@stud.techin.lt>
2025-12-21 19:05:33 +0200

    array task2

---
+++ Arrays/task2.js
@@ -0,0 +1,26 @@
+/* ##2 Declaring the array
+
+let myArray = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
+
+1. print the 3rd item here
+2. change the 'thursday' value to null here
+3. print the position of step 2 */
+"use strict";
+
+let myArray = [
+  "sunday",
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+];
+
+
+// 1.
+console.log(myArray[2]);
+// 2.
+myArray[4] = null;
+// 3.
+console.log(myArray[4]);
